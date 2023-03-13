6dd2b2e3bc08e45813f80ac66927864f4fedac00
Ryota <ryoenoki2013@gmail.com>
2023-03-13 10:42:59 +0900

    add: filters model

---
+++ lib/models/filters.dart
@@ -0,0 +1,16 @@
+import 'package:game_list/models/filters_year.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'filters.g.dart';
+
+@JsonSerializable()
+class Filters {
+  const Filters({
+    this.years,
+  });
+
+  final List<FiltersYear>? years;
+
+  factory Filters.fromJson(Map<String, dynamic> json) =>
+      _$FiltersFromJson(json);
+}
