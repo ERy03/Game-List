e36117a65dda5738982ad8c503effb59d6f23a39
Ryota <ryoenoki2013@gmail.com>
2023-05-18 07:28:29 +0900

    update: home page with multi bloc provider

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_list/home/pages/home_layout.dart';
+import 'package:game_list/home/widgets/category_widget/bloc/category_bloc.dart';
 import 'package:game_list/repository/game_repository.dart';
 import 'package:game_list/repository/implementation/game_implementation.dart';
 import 'package:game_list/service/game_service.dart';
@@ -14,11 +15,19 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: RepositoryProvider<GameRepository>(
-        create: (context) => GameImplementation(
-          GameServiceImpl(dio: Dio()),
-        ),
-        child: const HomeLayout(),
-      ),
+          create: (context) => GameImplementation(
+                GameServiceImpl(dio: Dio()),
+              ),
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) =>
+                    CategoryBloc(gameRepository: context.read<GameRepository>())
+                      ..add(GetCategories()),
+              ),
+            ],
+            child: const HomeLayout(),
+          )),
     );
   }
 }
