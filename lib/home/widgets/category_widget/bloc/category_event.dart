672b621e775f63d44a608631f7a658f95fbbf4ed
Ryota <ryoenoki2013@gmail.com>
2023-05-18 07:25:38 +0900

    add: category event

--- lib/home/widgets/category_widget/bloc/category_event.dart
+++ lib/home/widgets/category_widget/bloc/category_event.dart
@@ -1,4 +1,18 @@
 part of 'category_bloc.dart';
 
-@immutable
-abstract class CategoryEvent {}
+class CategoryEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class GetCategories extends CategoryEvent {}
+
+class SelectCategory extends CategoryEvent {
+  SelectCategory({
+    required this.idSelected,
+  });
+  final int idSelected;
+
+  @override
+  List<Object?> get props => [idSelected];
+}
