0176e7522aa8f7252c4e96fe230525e4ffc77b3c
Ryota <ryoenoki2013@gmail.com>
2023-05-18 07:26:44 +0900

    add: category bloc

--- lib/home/widgets/category_widget/bloc/category_bloc.dart
+++ lib/home/widgets/category_widget/bloc/category_bloc.dart
@@ -1,13 +1,46 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:game_list/models/genre.dart';
+import 'package:game_list/repository/game_repository.dart';
 
 part 'category_event.dart';
 part 'category_state.dart';
 
 class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
-  CategoryBloc() : super(CategoryInitial()) {
-    on<CategoryEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  CategoryBloc({
+    required this.gameRepository,
+  }) : super(const CategoryState()) {
+    on<GetCategories>(_mapGetCategoriesEventToState);
+    on<SelectCategory>(_mapSelectCategoryEventToState);
+  }
+  final GameRepository gameRepository;
+
+  void _mapGetCategoriesEventToState(
+      GetCategories event, Emitter<CategoryState> emit) async {
+    emit(state.copyWith(status: CategoryStatus.loading));
+    final genre = await gameRepository.getGenres();
+    genre.fold(
+      (l) {
+        debugPrint(l.toString());
+        emit(state.copyWith(status: CategoryStatus.error));
+      },
+      (r) => emit(
+        state.copyWith(
+          status: CategoryStatus.success,
+          categories: r,
+        ),
+      ),
+    );
+  }
+
+  void _mapSelectCategoryEventToState(
+      SelectCategory event, Emitter<CategoryState> emit) async {
+    emit(
+      state.copyWith(
+        status: CategoryStatus.selected,
+        idSelected: event.idSelected,
+      ),
+    );
   }
 }
