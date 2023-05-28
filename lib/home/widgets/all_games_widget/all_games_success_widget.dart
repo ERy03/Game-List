38d09642bb4a48ec0b9454373a64cd14857ed26d
Ryota <ryoenoki2013@gmail.com>
2023-05-28 16:39:03 +0900

    add: all games success widget

---
+++ lib/home/widgets/all_games_widget/all_games_success_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:game_list/home/widgets/all_games_widget/all_games_item.dart';
+import 'package:game_list/models/result.dart';
+
+class AllGamesSuccessWidget extends StatelessWidget {
+  const AllGamesSuccessWidget({
+    Key? key,
+    required this.games,
+  }) : super(key: key);
+
+  final List<Result> games;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.only(
+        top: 24.0,
+      ),
+      itemBuilder: (context, index) {
+        return AllGamesItem(
+          game: games[index],
+        );
+      },
+      separatorBuilder: (_, __) => const SizedBox(
+        height: 20.0,
+      ),
+      itemCount: games.length,
+    );
+  }
+}
